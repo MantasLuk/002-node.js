d48f71416f5a4e259d30cef44c5e8f994ade5cd7
Mantas Luk <mantasluk7@gmail.com>
2022-01-12 21:17:59 +0200

    final

--- files.js
+++ files.js
@@ -1,21 +1,23 @@
 console.clear();
 
-
 //kad skaityti failus naudojama papildoma biblioteka:
 const fs = require('fs/promises');
 
 (async () => {
-
-    
-
     const content = await fs.readFile('./data/drinks.txt', 'utf-8');
-    console.log(content);
-
-    for (const letter of content) {
-        console.log(letter);
+    // \r\n zymi tarpa
+    const drinksList = content.split('\r\n');
+    let i = 0;
+    for (const drink of drinksList) {
+        const drinkDetails = drink.split(' ');
+        // const name = drinkDetails[0];
+        // const price = drinkDetails[1];
+        const [name, price, count] = drinkDetails;
+        const moneyGet = parseFloat(price) * parseFloat(count);
+        // parseInt ==> kai string verciam i sveika skaiciu;
+        //parseFloat ==> kai i desimtaini.  
+        //arba toks variantas kai norim gauti tiesiog skaiciu: 
+        //                   const moneyGet = +price * +count;
+        console.log(`Pasirinkimas ${++i}: ${name}, kaina: ${price} EUR, parduota ${count} vienetu, apyvarta ${moneyGet.toFixed(2)} EUR`);
     }
-
-
 })();
-
-
